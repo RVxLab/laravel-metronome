fe17164afdab9bf3988bf72535a486fa5cca76ea
RVxLab <code@rvx.works>
2025-12-27 16:39:09 +1000

    feat: install Revolt and start setting up an event loop

--- src/Commands/StartCronlessScheduleCommand.php
+++ src/Commands/StartCronlessScheduleCommand.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace RVxLab\CronlessScheduler\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Events\Dispatcher;
+use Revolt\EventLoop;
 
 final class StartCronlessScheduleCommand extends Command
 {
@@ -12,8 +17,27 @@ final class StartCronlessScheduleCommand extends Command
 
     protected $description = 'Start the scheduler';
 
-    public function handle(): void
-    {
+    public function handle(
+        Schedule $schedule,
+        Dispatcher $dispatcher,
+        Cache $cache,
+        ExceptionHandler $exceptionHandler,
+    ): int {
         $this->info('Starting!');
+
+        $ticker = EventLoop::repeat(1, function (): void {
+            $this->info(sprintf("Tick: %s\n", date("Y-m-d H:i:s")));
+        });
+
+        EventLoop::onSignal(SIGINT, function () use ($ticker): never {
+            $this->newLine();
+            $this->warn('Received interrupt signal, preparing to exit');
+            EventLoop::cancel($ticker);
+            exit(self::SUCCESS);
+        });
+
+        EventLoop::run();
+
+        return self::SUCCESS;
     }
 }
