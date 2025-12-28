e49174e87307837f89995c8b2d008bae78cb8d59
RVxLab <code@rvx.works>
2025-12-28 15:10:41 +1000

    test: add some test logging to enture the event loop works

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\{Log, Schedule};
 
 Schedule::call(function (): void {
-    //
+    Log::info('This line is logged every minute');
 })->everyMinute()->description('every minute');
 
 Schedule::call(function (): void {
-    //
+    Log::info('This line is logged every 10 seconds');
 })->everyTenSeconds()->description('every ten seconds');
 
 Schedule::call(function (): void {
-    //
+    Log::info('This line is logged every five seconds');
 })->everyFiveSeconds()->description('every five seconds');
